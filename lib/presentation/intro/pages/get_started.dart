ab461dc91ef82166cb2d629cc1549ac98fc0874e
Mohammed <mohammed.hamidf18@komar.edu.iq>
2024-08-17 11:26:38 +0300

    refactor(GetStartedPage): remove column from the first container

--- lib/presentation/intro/pages/get_started.dart
+++ lib/presentation/intro/pages/get_started.dart
@@ -1,10 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:spotify/common/widgets/main_button.dart';
+import 'package:spotify/core/configs/assets/app_images.dart';
+import 'package:spotify/core/configs/assets/app_vectors.dart';
+import 'package:spotify/core/configs/theme/app_colors.dart';
+import 'package:spotify/presentation/mode_selection/pages/mode_selection.dart';
 
 class GetStartedPage extends StatelessWidget {
   const GetStartedPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Get Started"),),);
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage(AppImages.introBackground),
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.15),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 28.0),
+            child: SafeArea(
+              child: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: SvgPicture.asset(AppVectors.logo),
+                  ),
+                  const Spacer(),
+                  const Text(
+                    "Enjoy Listening to Music",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 18.0),
+                  ),
+                  const SizedBox(
+                    height: 17.0,
+                  ),
+                  const Text(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis enim purus sed phasellus. Cursus ornare id scelerisque aliquam.",
+                    style: TextStyle(
+                        fontWeight: FontWeight.w300,
+                        color: AppColors.lightGrey,
+                        fontSize: 17.0),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 17.0,
+                  ),
+                  MainButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (ctx) => const ModeSelectionPage(),
+                        ),
+                      );
+                    },
+                    title: "Get Started",
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
