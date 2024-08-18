938361374acbb3d79e03f6e62a874495403381a0
Mohammed <mohammed.hamidf18@komar.edu.iq>
2024-08-18 11:24:20 +0300

    feat(AppImages): add background image for login page

--- lib/core/configs/assets/app_images.dart
+++ lib/core/configs/assets/app_images.dart
@@ -2,4 +2,5 @@ class AppImages {
   static const String basePath = "assets/images/";
   static const String introBackground = "${basePath}intro_bg.png";
   static const String modePageBackground = "${basePath}choose_mode_bg.png";
+  static const String authBackground = "${basePath}auth_bg.png";
 }
