b6bbad8ac8d53fb1027ae2866adc6863b3483345
Mohammed <mohammed.hamidf18@komar.edu.iq>
2024-08-19 15:54:42 +0300

    feat(SignUpPage): create sign up page

--- lib/presentation/auth/pages/sign_up.dart
+++ lib/presentation/auth/pages/sign_up.dart
@@ -1,13 +1,132 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:spotify/common/widgets/basic_app_bar.dart';
+import 'package:spotify/common/widgets/main_button.dart';
+import 'package:spotify/core/configs/assets/app_vectors.dart';
+import 'package:spotify/core/configs/theme/app_colors.dart';
 
 class SignUpPage extends StatelessWidget {
-  const SignUpPage({super.key});
+  SignUpPage({super.key});
+
+  final TextEditingController _fullName = TextEditingController();
+  final TextEditingController _email = TextEditingController();
+  final TextEditingController _password = TextEditingController();
+
+  Widget _fullNameField(BuildContext context) {
+    return TextField(
+      controller: _fullName,
+      decoration: const InputDecoration(hintText: 'Full Name')
+          .applyDefaults(Theme.of(context).inputDecorationTheme),
+    );
+  }
+
+  Widget _emailField(BuildContext context) {
+    return TextField(
+      controller: _email,
+      decoration: const InputDecoration(hintText: 'Enter Email')
+          .applyDefaults(Theme.of(context).inputDecorationTheme),
+    );
+  }
+
+  Widget _passwordField(BuildContext context) {
+    return TextField(
+      controller: _password,
+      decoration: const InputDecoration(hintText: 'Password')
+          .applyDefaults(Theme.of(context).inputDecorationTheme),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [],
+      appBar: BasicAppBar(
+        title: SvgPicture.asset(
+          AppVectors.logo,
+          height: kToolbarHeight - 24,
+        ),
+      ),
+      body: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.all(28.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                "Register",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 14,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "If you need any support ",
+                    style: TextStyle(color: AppColors.darkGrey),
+                  ),
+                  Text(
+                    "click here",
+                    style: TextStyle(color: AppColors.primary),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              _fullNameField(context),
+              const SizedBox(
+                height: 20,
+              ),
+              _emailField(context),
+              const SizedBox(
+                height: 20,
+              ),
+              _passwordField(context),
+              const SizedBox(
+                height: 20,
+              ),
+              MainButton(onPressed: () {}, title: "Create Account"),
+              const SizedBox(
+                height: 20,
+              ),
+              const Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      endIndent: 15,
+                    ),
+                  ),
+                  Text("Or"),
+                  Expanded(
+                    child: Divider(
+                      indent: 15,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.mail, color: AppColors.darkGrey, size: 48,),
+                  SizedBox(width: 59,),
+                  Icon(Icons.apple, color: AppColors.darkGrey, size: 48,),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
